9afae49260dc5964977d5e5ced78a4318d1c2bab
Dstrasel <siruku@gmail.com>
2017-07-21 14:46:51 -0400

    Add jsdoc documentation.

--- getCredentials.js
+++ getCredentials.js
@@ -4,18 +4,23 @@ const jsforce = require('jsforce');
 const sfdc = require("./salesforce");
 const prompt = require('prompt');
 
-var schema = {
-    properties: {
-        tool: {
-            description: 'Enter your tool: Salesforce (C)ertificates or Salesforce (S)SO.',
-            required: true
-        },
-    }
-};
 
-
-
+/**
+ * Ask user whether they want to get certificates, or SSO metadata,
+ *
+ * @param {function} callback - Function which recives a string represnting the user selected tool.
+ * @return {function} callback - Returns the callback, causing it to begin calls the the Salesforce meta-data API.
+ */
 function promptUser(callback) {
+    // Defines what appears to the user in the terminal.
+    var schema = {
+        properties: {
+            tool: {
+                description: 'Enter your tool: Salesforce (C)ertificates or Salesforce (S)SO.',
+                required: true
+            },
+        }
+    };
     prompt.start();
     prompt.get(schema, function (error, result) {
         if (error) {
@@ -40,7 +45,16 @@ function promptUser(callback) {
     });
 }
 
+
+/**
+ * Read lines from a csv file and send parse user/password to the desired tool function.
+ *
+ * @param {string} fileName - The expected name of the csv file.
+ * @param {string} tool - Evaluated in reference to tool map in order to call the respective tool function.
+ * @return {null} None
+ */
 function getCSV(fileName, tool) {
+    // Open the targetted csv as a text stream.
     tool_map = {
         'certificate': sfdc.getSalesforceCertificates,
         'sso': sfdc.getSalesforceSSO
@@ -61,10 +75,13 @@ function getCSV(fileName, tool) {
 }
 
 
-
+/**
+ * Ask user for tool, and execute getCSV as callback upon tool selection.
+ *
+ * @return {null} None
+ */
 function main() {
     promptUser(getCSV);
-    //return getCSV('secret.csv', tool)
 }
 
 main();
